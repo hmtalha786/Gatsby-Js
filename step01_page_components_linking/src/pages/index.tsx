5f8724451609ce4888bc19706487e21ce3352973
Muhammad Talha <talhaofficialwork@gmail.com>
2021-12-11 12:54:17 +0500

    Update index.tsx

--- step01_page_components_linking/src/pages/index.tsx
+++ step01_page_components_linking/src/pages/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Link } from "gatsby";
+import { Link, navigate } from "gatsby";
 
 export default () => {
     return (
-        <div>
-            <p>We can link pages in Gatsby by using Link</p>
-            <Link to="/about/">About</Link>
-        </div>
+      <div>
+        <p>We can link pages in Gatsby by using Link and navigate</p>
+        <br />
+        <Link to="/about">About</Link>
+        <br />
+        <button
+          onClick={() => {
+            navigate("/about");
+          }}
+        >
+          Link To About
+        </button>
+      </div>
     );
 }
